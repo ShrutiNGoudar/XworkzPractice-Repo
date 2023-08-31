1006575879839ae4aaf48440929d5d0205b6b05f
ShrutiNGoudar <shrutiyxworkz@gmail.com>
2023-08-31 17:40:10 +0530

    initial commit

---
+++ src/main/java/org/training/AssignmentOnDataType.java
@@ -0,0 +1,111 @@
+package org.training;
+
+public class AssignmentOnDataType {
+    public static void main(String[] args) {
+        //Object Car
+        String CarName = "HondaCity";
+        String CarColor = "Red";
+        int CarLength = 3;
+        long CarPrice = 120000;
+        boolean CarTurnsOn = true;
+
+        System.out.println(CarName);
+        System.out.println(CarColor);
+        System.out.println(CarLength);
+        System.out.println(CarPrice);
+        System.out.println(CarTurnsOn);
+
+        //Object Engine
+        String EngineName = "Mercedes";
+        String EngineColor = "Black";
+        int EngineLength = 24;
+        long EnginePrice = 120000;
+        boolean EngineTurnsOn = true;
+
+        System.out.println(EngineName);
+        System.out.println(EngineColor);
+        System.out.println(EngineLength);
+        System.out.println(EnginePrice);
+        System.out.println(EngineTurnsOn);
+
+        //Object Vessel
+        String VesselName = "Pegion";
+        String Vessecolor = "Silver";
+        int VesselDiameter = 24;
+        long VesselPrice = 50000;
+        boolean VesselIsUsable = true;
+
+        System.out.println(VesselName);
+        System.out.println(Vessecolor);
+        System.out.println(VesselDiameter);
+        System.out.println(VesselPrice);
+        System.out.println(VesselIsUsable);
+
+        //Object Fridge
+        String FridgeName = "LG";
+        String FridgeColor = "White";
+        int FridgeHeight = 24;
+        long FridgePrice = 50000;
+        boolean FridgeIsUsable = true;
+
+        System.out.println(FridgeName);
+        System.out.println(FridgeColor);
+        System.out.println(FridgeHeight);
+        System.out.println(FridgePrice);
+        System.out.println(FridgeIsUsable);
+
+        //Object Bulb
+        String BulbName = "Philips";
+        String BulbColor = "Yellow";
+        String BulbHeight = "12mm";
+        int BulbPrice = 120;
+        boolean BulbIsUsable = false;
+
+        System.out.println(BulbName);
+        System.out.println(BulbColor);
+        System.out.println(BulbHeight);
+        System.out.println(BulbPrice);
+        System.out.println(BulbIsUsable);
+
+        //Object Vegetable
+        String VegitableName = "Beetroot";
+        String VegitableColor = "Red";
+        String VegitableWeight = "100gms";
+        int VegitablePrice = 120;
+        boolean VegitableIsUsable = true;
+
+        System.out.println(VegitableName);
+        System.out.println(VegitableColor);
+        System.out.println(VegitableWeight);
+        System.out.println(VegitablePrice);
+        System.out.println(VegitableIsUsable);
+
+        //Object Book
+        String BookName = "Vidya";
+        String BookColor = "Blue";
+        String BookWeight = "100Gms";
+        int BookPrice = 120;
+        boolean BookIsUsable = true;
+
+        System.out.println(BookName);
+        System.out.println(BookColor);
+        System.out.println(BookWeight);
+        System.out.println(BookPrice);
+        System.out.println(BookIsUsable);
+
+//Object Goggle
+        String GoggleName = "Gucci";
+        String GoggleColor = "Black";
+        int GoggleWeight = 200;
+        int GogglePrice = 120;
+        boolean GoggleIsUsable = true;
+
+        System.out.println(GoggleName);
+        System.out.println(GoggleColor);
+        System.out.println(GoggleWeight);
+        System.out.println(GogglePrice);
+        System.out.println(GoggleIsUsable);
+
+
+    }
+}
